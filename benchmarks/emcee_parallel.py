1ac8673348d08aa62f7819e5156f43a2a3b8e7b0
Timothy Crundall <u5018130@anu.edu.au>
2019-09-25 11:24:15 +0200

    Made emcee parallel consistent with python 2

--- benchmarks/emcee_parallel.py
+++ benchmarks/emcee_parallel.py
@@ -4,20 +4,24 @@ https://emcee.readthedocs.io/en/latest/tutorials/parallel/
 credit: Dan Foreman-Mackey
 '''
 import os
-os.environ["OMP_NUM_THREADS"] = "1"
-
 from multiprocessing import cpu_count
+import emcee
+import time
+import numpy as np
+import sys
+
+os.environ["OMP_NUM_THREADS"] = "1"
+print(emcee.__version__)
 ncpu = cpu_count()
 print("{0} CPUs".format(ncpu))
 
-import emcee
-print(emcee.__version__)
-
-import time
-import numpy as np
+if len(sys.argv) > 1:
+    scale = float(sys.argv[1])
+else:
+    scale = 1.0
+print('Scale factor of {}'.format(scale))
 
 def log_prob(theta):
-    scale = 0.3
     t = time.time() + scale*np.random.uniform(0.005, 0.008)
     while True:
         if time.time() >= t:
@@ -38,11 +42,15 @@ print("Serial took {0:.1f} seconds".format(serial_time))
 
 from multiprocessing import Pool
 
-with Pool() as pool:
-    sampler = emcee.EnsembleSampler(nwalkers, ndim, log_prob, pool=pool)
-    start = time.time()
-    sampler.run_mcmc(initial, nsteps)
-    end = time.time()
-    multi_time = end - start
-    print("Multiprocessing took {0:.1f} seconds".format(multi_time))
-    print("{0:.1f} times faster than serial".format(serial_time / multi_time))
+# with Pool() as pool: (not consistent with python2)
+pool = Pool()
+
+sampler = emcee.EnsembleSampler(nwalkers, ndim, log_prob, pool=pool)
+start = time.time()
+sampler.run_mcmc(initial, nsteps)
+end = time.time()
+multi_time = end - start
+print("Multiprocessing took {0:.1f} seconds".format(multi_time))
+print("{0:.1f} times faster than serial".format(serial_time / multi_time))
+
+pool.close()
