e6635d93821d5a51dc35c2a43cd7f19686a2382f
Timothy Crundall <u5018130@anu.edu.au>
2019-10-02 12:37:08 +0200

    Add prepare data helper script

---
+++ scripts/prepare_data.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+"""
+A helper script that prepares data for input into Chronostar.
+Accepts as a command line argument a path to a parameter file
+(see README.md for how to structure the parameter file and
+various available parameters).
+"""
+
+import os.path
+import sys
+sys.path.insert(0, '..')
+
+from chronostar import datatool
+
+if len(sys.argv[1]) != 2:
+    raise UserWarning('Incorrect usage. Path to parameter file is required'
+                      ' as a single command line argument. e.g.\n'
+                      '   > python prepare_data.py path/to/parsfile.par')
+
+data_par_file = sys.argv[1]
+
+if not os.path.isfile(data_par_file):
+    raise UserWarning('Provided file does not exist')
+
+datatool.prepare_data(data_par_file)
