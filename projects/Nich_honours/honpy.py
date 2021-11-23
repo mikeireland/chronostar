6cf8c3d7a5408e149f5c72ed559dbf9c222b4e2b
NichStanden <u6663520@anu.edu.au>
2021-11-23 11:53:22 +1100

    honpy.py tweaks
    Gives keyword args through all the functions, renames some variables to at least 3 characters. g_kernal_den now hopefully normalises properly (with an integral rather than a sum)

--- projects/Nich_honours/honpy.py
+++ projects/Nich_honours/honpy.py
@@ -2,13 +2,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.spatial as sp
-#import scipy.integrate as integrate
+import scipy.integrate as integrate
 
 g, bp , rp, age, bn, feh, m, ms = np.load('10MSTAR_POPULATION_GBRABFM.npy').T
 
 def show_pop():
     fig, ax = plt.subplots()
-    im = ax.scatter((bp-rp),g,c=age,s=1, alpha=1)
+    im = ax.scatter((bp-rp),g,c=age,s=1, alpha=0.9)
     fig.colorbar(im, ax=ax)
     ax.set_ylabel('G')
     ax.set_xlabel('Gaia B-R')
@@ -22,7 +22,8 @@ zipnowork=np.vstack(((bp-rp),g)).T
 zipnowork.shape
 tree=sp.KDTree(zipnowork)
 
-def treeGetAges(col,mag,radius,data=tree):
+#%%
+def treeGetAges(col,mag,radius, data=tree):
 
     inds=data.query_ball_point((col,mag),radius)
     ages=age[inds]
@@ -33,43 +34,53 @@ def treeGetAges(col,mag,radius,data=tree):
 
 lgage=np.arange(5,11.4, 0.1)
 
-def make_hists(c,g,n=100,r=0.01):
-    ages,colres,gres=treeGetAges(c, g, r)
+def make_hists(col, gmag, n=100, r=0.01, data=tree):
+    ages,colres,gres=treeGetAges(col, gmag, r, data=data)
     if len(ages)>(n*2):
         r=0.75*r
-        ages,colres,gres=treeGetAges(c, g, r)
+        ages,colres,gres=treeGetAges(col, gmag, r, data=data)
     if len(ages)<n:
-        r=1.2*r
-        ages,colres,gres=treeGetAges(c, g, r)
+        r=1.5*r
+        ages,colres,gres=treeGetAges(col, gmag, r, data=data)
     
-    dists=np.sqrt((c-colres)**2+(g-gres)**2)
+    dists=np.sqrt((col-colres)**2+(gmag-gres)**2)
     ages=ages[np.argsort(dists)]
     ages=ages[:n]
     ags=np.round((ages-5)/0.1).astype(int)
     age_hists=np.zeros_like(lgage)
     for i in ags:
         age_hists[i]+=1
-    print(len(ages))
     return age_hists    
     
 gaus  = np.exp(-(lgage-np.median(lgage))**2/ 0.1**2 /2)
 gaus /= np.sum(gaus)
 gausft = np.fft.rfft(np.fft.fftshift(gaus))
 
-def g_kernal_den(c,g):
-    age_h=make_hists(c, g)
-    age_pdf=np.fft.irfft(np.fft.rfft(age_h)*gausft)
-   # grated=integrate.simps(age_pdf)
-    normed=age_pdf*10**lgage/sum(age_pdf*10**lgage)
-    plt.plot(lgage,normed)
+def g_kernal_den(col, gmag, n=100, r=0.01, data=tree, 
+                 show_PDF=False, show_NearPop=True):
+    age_h=make_hists(col, gmag, n=n, r=r, data=data);
+    age_pdf=np.fft.irfft(np.fft.rfft(age_h)*gausft);
+    grated=integrate.simps(age_pdf*10**lgage);
+    normed=age_pdf*10**lgage/grated;
+    if show_PDF:
+         fig, ax = plt.subplots()
+         ax.plot(lgage,normed)
+         ax.set_ylabel('Relative Probability')
+         ax.set_xlabel('log(Age)')
+         fig.savefig('g_kernal_den_OUT.pdf')
+         fig.show
     return normed
 
-def get_probage(c,g,A):
+def get_probage(col,gmag,A, n=100, r=0.01, data=tree, 
+                show_PDF=False, show_NearPop=False):
      if not(5<A<11.4):
         print('Err; log(age) out of 5 to 11.4 interval')
-     pdf=g_kernal_den(c, g)
-     iclosest=(np.abs(lgage-A)).argmin()
-     return pdf[iclosest]
-   
-    
-#if __name__=='__main__':
+     pdf=g_kernal_den(col, gmag, n=n, r=r, data=data, 
+                      show_PDF=show_PDF,show_NearPop=show_NearPop)
+     for i in range(len(lgage)):
+         if lgage[i+1] > A:
+             grad=(pdf[i+1]-pdf[i])/(lgage[i+1]-lgage[i])
+             prob=grad*(A-lgage[i])+pdf[i]
+             break
+        
+     return prob
